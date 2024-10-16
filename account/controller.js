9ec1b62bbc49c0aa22fd7eab68695fd0cd6ccdf3
Mike Haiduk <x29a100@gmail.com>
2024-10-16 19:26:50 +0300

    mfm-angular-template

--- account/controller.js
+++ account/controller.js
@@ -1,11 +1,5 @@
 function openAccount(success) {
-    window.$mdDialog.show({
-        templateUrl: '/angular-example/account/index.html',
-        controller: function ($scope) {
-            addFormats($scope)
-        }
-    }).then(function () {
-        if (success)
-            success()
+    showDialog('/angular-example/account/index.html', success, function ($scope) {
+        addFormats($scope)
     })
 }
