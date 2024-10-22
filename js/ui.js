bd9f28c5d845515032d70488880803f24d8fe70a
Mike Haiduk <x29a100@gmail.com>
2024-10-22 14:48:34 +0300

    add md viewer

--- js/ui.js
+++ js/ui.js
@@ -124,3 +124,44 @@ function showSuccess(message, success) {
     if (success)
         success(message)
 }
+
+async function loadMarkdown(divId, url) {
+    try {
+        const response = await fetch(url);
+        const text = await response.text();
+
+        var defaults = {
+            html: false, // Enable HTML tags in source
+            xhtmlOut: false, // Use '/' to close single tags (<br />)
+            breaks: false, // Convert '\n' in paragraphs into <br>
+            langPrefix: 'language-', // CSS language prefix for fenced blocks
+            linkify: true, // autoconvert URL-like texts to links
+            typographer: true, // Enable smartypants and other sweet transforms
+            // options below are for demo only
+            _highlight: true, // <= THIS IS WHAT YOU NEED
+            _strict: false,
+            _view: 'html' // html / src / debug
+        };
+
+        defaults.highlight = function (str, lang) {
+            var esc = md.utils.escapeHtml;
+            console.log(str)
+            console.log(lang)
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return '<pre class="hljs"><code>' +
+                        hljs.highlight(lang, str, true).value +
+                        '</code></pre>';
+                } catch (__) {}
+            }else{
+                return '<pre class="hljs"><code>' + esc(str) + '</code></pre>';
+            }
+
+        };
+
+        let md = window.markdownit(defaults)
+        document.getElementById(divId).innerHTML = md.render(text);
+    } catch (error) {
+        console.error('Ошибка загрузки данных:', error);
+    }
+}
