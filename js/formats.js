0730ced7f0416a8e142c7d034955e6fb4e699dbe
Mike Haiduk <x29a100@gmail.com>
2024-11-23 14:39:30 +0100

    copy change

--- js/formats.js
+++ js/formats.js
@@ -178,29 +178,6 @@ function addFormats($scope) {
         return result;
     }
 
-    $scope.copyText = function (text) {
-        var textArea = document.createElement("textarea");
-        textArea.value = text;
-        // Avoid scrolling to bottom
-        textArea.style.top = "0";
-        textArea.style.left = "0";
-        textArea.style.position = "fixed";
-
-        document.body.appendChild(textArea);
-        textArea.focus();
-        textArea.select();
-
-        try {
-            var successful = document.execCommand('copy');
-            var msg = successful ? 'successful' : 'unsuccessful';
-            console.log('Fallback: Copying text command was ' + msg);
-        } catch (err) {
-            console.error('Fallback: Oops, unable to copy', err);
-        }
-
-        document.body.removeChild(textArea);
-    }
-
     $scope.getLogoLink = function (domain) {
         return "https://storage.mytoken.space/" + domain + ".png"
     }
